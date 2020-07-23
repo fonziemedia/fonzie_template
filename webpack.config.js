370e779b9b99f4c1d4374033bc179a4b89055393
Carlos Fonseca <cffonseca@gmail.com>
2020-07-23 12:00:57 +0200

    General template fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
-const path = require('path');
-const webpack = require('webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin'); // Required to generate html/php build file using src templates
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Required to generate html/php build file using src templates
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   performance: {
     hints: false
   },
-  devtool: 'source-map',
-  context: path.resolve(__dirname, './src/js'), //__dirname refers to the directory where this webpack.config.js lives
+  devtool: "source-map",
+  context: path.resolve(__dirname), //__dirname refers to the directory where this webpack.config.js lives
   entry: {
-    app: './app.js',
+    app: "./src/js/app.js"
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, './build'),
-    publicPath: '', // path that will be set on link href and script src html tags
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "" // path that will be set on link href and script src html tags
   },
   // devServer: {
   //   contentBase: path.resolve(__dirname, './src/js'),
   //   watchContentBase: true,
   // },
   plugins: [
+    new HtmlWebpackPlugin({ title: "Potus", template: "./index.html" }),
     // new HtmlWebpackPlugin({
-    //   title: 'Wildfire Template',
-    //   filename: 'index.php', // default value is index.html
+    //   title: "Wildfire Template",
+    //   filename: "index.php", // default value is index.html
     //   // minify: {
     //   //   collapseWhitespace: true
     //   // },
     //   hash: true,
-    //   template: 'index.php',
+    //   template: "index.php"
     // }),
     // new HtmlWebpackPlugin({
     //   title: 'Wildfire Template - About',
@@ -42,15 +44,18 @@ module.exports = {
     // }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[name].bundle.css',
+      // both options are.. optional
+      filename: "[name].bundle.css",
       chunkFilename: "[id].css"
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
       "window.jQuery": "jquery",
-      Popper: ['popper.js', 'default'],
+      Popper: ["popper.js", "default"]
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "assets", to: "assets" }]
     })
   ],
   module: {
@@ -58,12 +63,14 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: [/node_modules/], //add any legacy code here (optional for speed)
-        use: [{
-          loader: 'babel-loader', //babel compiles es2015/6/7 (new) javascript to 'old' javascript for old browsers
-          options: {
-            presets: ['@babel/preset-env'] //read more about presets, should we be using 'es2015'?
-          },
-        }],
+        use: [
+          {
+            loader: "babel-loader", //babel compiles es2015/6/7 (new) javascript to 'old' javascript for old browsers
+            options: {
+              presets: ["@babel/preset-env"] //read more about presets, should we be using 'es2015'?
+            }
+          }
+        ]
       },
       {
         test: /\.scss$/,
@@ -81,13 +88,14 @@ module.exports = {
             options: {
               // you can specify a publicPath here
               // by default it use publicPath in webpackOptions.output
-              publicPath: '../'
+              publicPath: "../"
             }
           },
-          'css-loader', 'sass-loader'
+          "css-loader",
+          "sass-loader"
         ]
-      },
+      }
       // Loaders for other file types can go here
-    ],
+    ]
   }
 };
